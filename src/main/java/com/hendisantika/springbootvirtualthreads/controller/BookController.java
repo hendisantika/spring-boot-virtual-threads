55021e34a3a878cfcade5ff3a1f59d741c9abff8
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 11:23:10 +0700

    Add getAll in BookController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootvirtualthreads/controller/BookController.java
+++ src/main/java/com/hendisantika/springbootvirtualthreads/controller/BookController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.springbootvirtualthreads.controller;
 
+import com.hendisantika.springbootvirtualthreads.dto.BookDTO;
 import com.hendisantika.springbootvirtualthreads.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookRepository bookRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<BookDTO>> getAll() {
+        UUID uuid = UUID.randomUUID();
+        log.info("getAll() {} running", uuid);
+
+        ResponseEntity<List<BookDTO>> list = ResponseEntity.ok(this.bookRepository.findAll().stream().
+                map((e) -> new BookDTO(e.getBookId(), e.getAuthor(), e.getIsbn(), e.getTitle(), e.getYear())).collect(Collectors.toList()));
+
+        log.info("getAll() {} executed", uuid);
+        return list;
+    }
 }
