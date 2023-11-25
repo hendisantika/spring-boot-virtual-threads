16796a3ba8431914efb4d1b495598191cad92244
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 11:20:44 +0700

    Add OrderRepository 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootvirtualthreads/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootvirtualthreads.repository;
+
+import com.hendisantika.springbootvirtualthreads.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-virtual-threads
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/25/23
+ * Time: 11:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
