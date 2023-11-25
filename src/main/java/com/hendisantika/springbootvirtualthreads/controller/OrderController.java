601a83d21f662d232402573c87f4a97236c6fbe1
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 11:26:20 +0700

    Add addOrder in OrderController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootvirtualthreads/controller/OrderController.java
+++ src/main/java/com/hendisantika/springbootvirtualthreads/controller/OrderController.java
@@ -1,12 +1,23 @@
 package com.hendisantika.springbootvirtualthreads.controller;
 
+import com.hendisantika.springbootvirtualthreads.domain.Book;
+import com.hendisantika.springbootvirtualthreads.domain.Order;
+import com.hendisantika.springbootvirtualthreads.domain.User;
+import com.hendisantika.springbootvirtualthreads.dto.OrderDTO;
 import com.hendisantika.springbootvirtualthreads.repository.BookRepository;
 import com.hendisantika.springbootvirtualthreads.repository.OrderRepository;
 import com.hendisantika.springbootvirtualthreads.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +40,27 @@ public class OrderController {
     private final BookRepository bookRepository;
 
     private final OrderRepository orderRepository;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Transactional
+    public ResponseEntity<OrderDTO> addOrder(@RequestParam("bookIsbn") String bookIsbn, @RequestParam("firstName") String firstName) {
+
+        UUID uuid = UUID.randomUUID();
+        log.info("addOrder() {} running", uuid);
+
+        User user = userRepository.findByFirstName(firstName);
+        Book book = bookRepository.findByIsbn(bookIsbn);
+        log.info("addOrder() {} I've got user and book", uuid);
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setQuantity(1);
+        order.setBook(book);
+        order = orderRepository.save(order);
+
+        OrderDTO orderDTO = new OrderDTO(order.getOrderId(), order.getQuantity(), book.getBookId(), user.getUserId());
+        log.info("addOrder() {} executed", uuid);
+        return ResponseEntity.ok(orderDTO);
+
+    }
 }
