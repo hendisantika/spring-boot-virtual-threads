2e4d9466a880cef2993689b0d8881f69745b1d3f
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 11:08:51 +0700

    Initial project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootvirtualthreads/SpringBootVirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootvirtualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVirtualThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVirtualThreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootvirtualthreads/SpringBootVirtualThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootvirtualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVirtualThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
